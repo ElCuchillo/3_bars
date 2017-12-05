1d29c02d56ae2cefaa24d62917092c15e7c07b37
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2017-12-05 12:51:30 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -9,15 +9,16 @@ def load_data(filepath):
 
 
 def get_biggest_bar(bar_data):
-    the_biggest_bar = max(bar_data['features'], key=lambda x:
-    x['properties']['Attributes']['SeatsCount'])
-
+    the_biggest_bar = max(bar_data['features'],
+                          key=lambda x: x['properties']['Attributes']
+                                         ['SeatsCount'])
     return the_biggest_bar
 
 
 def get_smallest_bar(bar_data):
-    the_smallest_bar = min(bar_data['features'], key=lambda x:
-    x['properties']['Attributes']['SeatsCount'])
+    the_smallest_bar = min(bar_data['features'],
+                           key=lambda x: x['properties']['Attributes']
+                                          ['SeatsCount'])
 
     return the_smallest_bar
 
@@ -27,20 +28,17 @@ def get_distance(source_point, some_bar):
     earth_radius = 6373
 
     delta_latitude = (radians(some_bar['geometry']['coordinates'][latitude]
-                              - source_point[latitude]))
+                      - source_point[latitude]))
     delta_longitude = (radians(some_bar['geometry']['coordinates'][longitude]
-                               - source_point[longitude]))
+                       - source_point[longitude]))
     # the formula for the distance is calculated by Lexander
     #    (lizz4mail@gmail.com)  based on Pifagorean theorem wit the assumptions
     #    that the Earth is round in general, and on the scale of the Moscow
     #    region it is flat.
 
     distance = (earth_radius * sqrt(delta_latitude ** 2
-                                    + (delta_longitude
-                                       * cos(radians(some_bar['geometry']
-                                                             ['coordinates']
-                                                             [latitude])))
-                                    ** 2))
+        + (delta_longitude * cos(radians(some_bar['geometry']['coordinates']
+            [latitude]))) ** 2))
     return distance
 
 
@@ -55,48 +53,48 @@ def get_closest_bar(bar_data, longitude, latitude):
 
 
 def input_coordinates():
-    print('Для нахождения ближайшего бара введите текущие gps-координаты'
-          'в десятичном формате.')
+    print('\nДля нахождения ближайшего бара введите текущие gps-координаты'
+          ' в десятичном формате.')
     try:
         longitude = float(input('долгота:'))
         latitude = float(input('широта:'))
         return longitude, latitude
-
     except ValueError:
-        return 0, 0
+        print('\nНеверный формат gps-координат')
+        return None, None
 
 
-def get_bar_properties(some_bar):
-    bar_properties = (some_bar['properties']['Attributes']['Name'],
-                      some_bar['properties']['Attributes']['Address'],
-                      some_bar['properties']['Attributes']['SeatsCount'])
-    return bar_properties
-
-def output_results(biggest_bar, smallest_bar, closest_bar='', min_distance=0):
-    print('\nСамый большой бар - "{0[0]}, {0[1]}", {0[2]} мест'.
-          format(get_bar_properties(biggest_bar)))
-    print('\nСамый маленький бар - "{0[0]}, {0[1]}", {0[2]} мест'.
-          format(get_bar_properties(smallest_bar)))
-    if closest_bar:
-        print('\nБлижайший бар - "{0[0]}, {0[1]}", {0[2]} мест, '
-              'расстояние {1:.2f} км'.
-              format(get_bar_properties(closest_bar), min_distance))
+def print_bar_info(some_bar, bar_type, min_distance=None):
+    if not some_bar:
+        message = '\nБлижайший бар найти не удалось, т.к. не введены ' \
+                  'текущие координаты.'
     else:
-        print('\nБлижайший бар найти не удалось, т.к. не введены '
-              'текущие координаты.')
+        bar_attributes = (some_bar['properties']['Attributes']['Name'],
+                          some_bar['properties']['Attributes']['Address'],
+                          some_bar['properties']['Attributes']['SeatsCount'])
+        message = '\n{0} бар - "{1[0]}, {1[1]}", '.format(bar_type,
+                                                          bar_attributes)
+        if min_distance:
+            message += 'расстояние {:.2f}км'.format(min_distance)
+        else:
+            message += '{} мест'.format(bar_attributes[2])
+    print(message)
 
 
 if __name__ == '__main__':
     try:
         bar_data = load_data(sys.argv[1])
         biggest_bar = get_biggest_bar(bar_data)
+        print_bar_info(biggest_bar, 'Самый большой')
         smallest_bar = get_smallest_bar(bar_data)
+        print_bar_info(smallest_bar, 'Самый маленький')
         current_longitude, current_latitude = input_coordinates()
         closest_bar, min_distance = get_closest_bar(bar_data,
                                                     current_longitude,
                                                     current_latitude)
-        output_results(biggest_bar, smallest_bar, closest_bar, min_distance)
+        print_bar_info(closest_bar, 'Ближайший', min_distance)
+
     except FileNotFoundError as error:
         print(error)
     except IndexError:
-        print("Using: python3 bars.py <path to file>")
+        print("Использование: python3 bars.py <path to file>")
